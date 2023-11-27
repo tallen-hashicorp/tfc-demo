a96bb1af696272875d2f07fc993a3222f0041d48
Tyler Allen <tyler.allen@hashicorp.com>
2023-11-27 21:05:23 +0000

    Add some provider stuff

--- cloud-vcs-vault/main.tf
+++ cloud-vcs-vault/main.tf
@@ -13,6 +13,29 @@ terraform {
   }
 }
 
+provider "vault" {
+  // skip_child_token must be explicitly set to true as TFC manages the token lifecycle
+  skip_child_token = true
+  address          = var.tfc_vault_dynamic_credentials.default.address
+  namespace        = var.tfc_vault_dynamic_credentials.default.namespace
+
+  auth_login_token_file {
+    filename = var.tfc_vault_dynamic_credentials.default.token_filename
+  }
+}
+
+provider "vault" {
+  // skip_child_token must be explicitly set to true as TFC manages the token lifecycle
+  skip_child_token = true
+  alias            = "ALIAS1"
+  address          = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].address
+  namespace        = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].namespace
+
+  auth_login_token_file {
+    filename = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].token_filename
+  }
+}
+
 provider "aws" {
   shared_credentials_files = [var.tfc_vault_backed_aws_dynamic_credentials.default.shared_credentials_file]
   region = "us-east-1"
--- cloud-vcs-vault/variables.tf
+++ cloud-vcs-vault/variables.tf
@@ -9,3 +9,21 @@ variable "tfc_vault_backed_aws_dynamic_credentials" {
     }))
   })
 }
+
+variable "tfc_vault_dynamic_credentials" {
+  description = "Object containing Vault dynamic credentials configuration"
+  type = object({
+    default = object({
+      token_filename = string
+      address = string
+      namespace = string
+      ca_cert_file = string
+    })
+    aliases = map(object({
+      token_filename = string
+      address = string
+      namespace = string
+      ca_cert_file = string
+    }))
+  })
+}
