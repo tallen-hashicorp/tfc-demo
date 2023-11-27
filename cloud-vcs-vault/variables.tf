a80794f3c2fd504ff6627671961aec6462e674da
Tyler Allen <tyler.allen@hashicorp.com>
2023-11-27 20:40:24 +0000

    Add shared_credentials_files

--- cloud-vcs-vault/variables.tf
+++ cloud-vcs-vault/variables.tf
@@ -1,17 +1,11 @@
-variable "tfc_vault_dynamic_credentials" {
-  description = "Object containing Vault dynamic credentials configuration"
+variable "tfc_vault_backed_aws_dynamic_credentials" {
+  description = "Object containing Vault-backed AWS dynamic credentials configuration"
   type = object({
     default = object({
-      token_filename = string
-      address = string
-      namespace = string
-      ca_cert_file = string
+      shared_credentials_file = string
     })
     aliases = map(object({
-      token_filename = string
-      address = string
-      namespace = string
-      ca_cert_file = string
+      shared_credentials_file = string
     }))
   })
 }
